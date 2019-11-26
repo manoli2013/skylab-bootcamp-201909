f86d7541edaf053aadacc5d5d0d580c11e5d27e4
Manoli 2013 <websitesbypol@gmail.com>
2019-11-26 11:12:11 +0100

    update api

---
+++ staff/noli-pascual/tasks/tasks-app/build/precache-manifest.98268ef333ff5d8626cbe26af9d2e0ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28b01b8f7baf695323a778a5117dc87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd6b888fcb9f78ac3d68",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6ffd3f314c3a3777f54b",
+    "url": "/static/js/2.4966ee3a.chunk.js"
+  },
+  {
+    "revision": "fd6b888fcb9f78ac3d68",
+    "url": "/static/js/main.00ba8319.chunk.js"
+  },
+  {
+    "revision": "206a8d54cac43bd3210a",
+    "url": "/static/js/runtime-main.e8c53046.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
